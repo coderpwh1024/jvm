b24cd76239c5a8c6ec2adf25655e0106ad5ae2a5
coder-PengWenHao <pengwenhao@aliyun.com>
2022-04-10 17:33:34 +0800

    add JavaVMStackSOF

---
+++ src/main/java/com/coderpwh/jvm/chapter2/JavaVMStackSOF.java
@@ -0,0 +1,20 @@
+package com.coderpwh.jvm.chapter2;
+
+public class JavaVMStackSOF {
+
+    private  int stackLength=1;
+
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();;
+    }
+
+    public static void main(String[] args) {
+
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        oom.stackLeak();
+
+    }
+
+
+}
