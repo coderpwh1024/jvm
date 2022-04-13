9edd9915d6e9fdc6bf6f3fb0791ec5a5ede16006
coder-PengWenHao <pengwenhao@aliyun.com>
2022-04-13 22:33:22 +0800

    feat:add RuntimeConstantPoolOOM2

---
+++ src/main/java/com/coderpwh/jvm/chapter2/RuntimeConstantPoolOOM2.java
@@ -0,0 +1,23 @@
+package com.coderpwh.jvm.chapter2;
+
+public class RuntimeConstantPoolOOM2 {
+
+
+    public static void main(String[] args) {
+
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+
+        System.out.println(str1.intern()==str1);
+
+
+
+        String str2 = new StringBuilder("java").append("jvm").toString();
+
+        System.out.println(str2.intern()==str2);
+
+
+    }
+
+
+
+}
