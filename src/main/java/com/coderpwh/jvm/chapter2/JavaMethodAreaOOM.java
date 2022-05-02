ca4015b0ef2523a90820d5d59a4f8816dfe8d307
coder-PengWenHao <pengwenhao@aliyun.com>
2022-05-02 16:08:57 +0800

    feat:add JavaMethodAreaOOM

---
+++ src/main/java/com/coderpwh/jvm/chapter2/JavaMethodAreaOOM.java
@@ -0,0 +1,33 @@
+package com.coderpwh.jvm.chapter2;
+
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class JavaMethodAreaOOM {
+
+    public static void main(String[] args) {
+
+        while (true) {
+
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(HeapOOM.OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+
+                    return methodProxy.invokeSuper(objects, args);
+                }
+            });
+            enhancer.create();
+        }
+
+
+    }
+
+
+}
