8ed2a93ccd81c2456682ab04a4854131b89ddfe9
coder-PengWenHao <pengwenhao@aliyun.com>
2022-05-04 16:15:15 +0800

    fix:update ConstClass class remark

--- src/main/java/com/coderpwh/jvm/chapter7/ConstClass.java
+++ src/main/java/com/coderpwh/jvm/chapter7/ConstClass.java
@@ -1,5 +1,10 @@
 package com.coderpwh.jvm.chapter7;
 
+
+/***
+ *   下面的HELLOWORLD  常量在编译阶段已经存入NotInitialization类的常量池中，
+ *   本质上并没有直接引用到定义常量的类，因此不会触发定义常量的类初始化中
+ */
 public class ConstClass {
 
     static {
