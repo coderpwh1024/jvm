fe023fe32819bc58ef7cccee4623ca3678dcee65
coder-PengWenHao <pengwenhao@aliyun.com>
2022-05-04 15:29:42 +0800

    fix NotInitialization class

--- src/main/java/com/coderpwh/jvm/chapter7/NotInitialization.java
+++ src/main/java/com/coderpwh/jvm/chapter7/NotInitialization.java
@@ -3,6 +3,10 @@ package com.coderpwh.jvm.chapter7;
 public class NotInitialization {
 
     public static void main(String[] args) {
-        System.out.println(SubClass.value);
+//        System.out.println(SubClass.value);
+
+         //  newArray 加载初始化
+        SuperClass[] sca = new SuperClass[10];
+
     }
 }
