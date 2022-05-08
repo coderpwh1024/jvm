982d3a01dd53ad3c8191b7e1392fa0fea5d11ff0
coder-PengWenHao <pengwenhao@aliyun.com>
2022-05-08 15:51:11 +0800

    feat:add Sub class

---
+++ src/main/java/com/coderpwh/jvm/chapter7/Sub.java
@@ -0,0 +1,14 @@
+package com.coderpwh.jvm.chapter7;
+
+public class Sub  extends  Parent{
+
+
+     public static  int B=A;
+
+    public static void main(String[] args) {
+
+        System.out.println(Sub.B);
+    }
+
+
+}
