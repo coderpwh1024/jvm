485fb36c3613788a0f0b9505dd2a9faa50b28a82
coder-PengWenHao <pengwenhao@aliyun.com>
2022-05-08 15:38:09 +0800

    fix:update Test class

--- src/main/java/com/coderpwh/jvm/chapter7/Test.java
+++ src/main/java/com/coderpwh/jvm/chapter7/Test.java
@@ -2,13 +2,14 @@ package com.coderpwh.jvm.chapter7;
 
 public class Test {
 
+    static  int i =1;
+
+
     static {
 
         i=0;
 
-        System.out.println(i);
+        System.out.println(i);   // 编译不通过，非法引用。将下面 static int i=1; 移到代码块前可以使用
     }
 
-
-    static  int i =1;
 }
